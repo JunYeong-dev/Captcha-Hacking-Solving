0f69a3aed37d8f9bfd2b7c554906b63865c20424
Jun-cloud <kjy8827@gmail.com>
2020-05-13 22:15:20 +0900

    Create fuction get_result extract number and print

--- run.py
+++ run.py
@@ -12,8 +12,36 @@ with np.load(FILE_NAME) as data:
 knn = cv2.ml.KNearest_create()
 knn.train(train, cv2.ml.ROW_SAMPLE, train_labels)
 
-def check(test, train, train_labels):
+def check(test):
     # 가장 가까운 k개의 글자를 찾아, 어떤 숫자에 해당하는지 찾음(테스트 데이터 개수에 따라서 조절)
     # 이 예제에서는 같은 숫자의 경우 이미지가 모두 같기 때문에 가장 가까운 이미지를 하나만 찾아도 분류가 가능
-    ret, result, neighbours, dist = knn.findNearst(test, k=1)
+    ret, result, neighbours, dist = knn.findNearest(test, k=1)
     return result
+
+def get_result(file_name):
+    image = cv2.imread(file_name)
+    # 각각의 이미지를 chars변수에 담음
+    chars = utils.extract_chars(image)
+    result_string = ""
+
+    for char in chars:
+        # 하나하나의 숫자 혹은 기호
+        matched = check(utils.resize20(char[1]))
+
+        # 숫자
+        if matched < 10:
+            result_string += str(int(matched))
+            continue
+        # 기호
+        if matched == 10:
+            matched = '+'
+        elif matched == 11:
+            matched = '-'
+        elif matched == 12:
+            matched = '*'
+
+        result_string += matched
+
+    return result_string
+
+print(get_result("1.png"))
